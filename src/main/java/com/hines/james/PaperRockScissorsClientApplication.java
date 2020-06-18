25f944bbdb0eef12b64e2e236ad8324402670bf7
James Hines <jameshines10@gmail.com>
2020-06-17 20:18:51 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/PaperRockScissorsClientApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperRockScissorsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperRockScissorsClientApplication.class, args);
+	}
+
+}
